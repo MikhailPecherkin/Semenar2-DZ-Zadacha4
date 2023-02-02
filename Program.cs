d6da384b1c1571d9a472cb0f4c9eb0c81b58c730
MikhailPecherkin <m.pechiorkin@yandex.ru>
2023-02-02 11:50:05 +0300

    Test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c) 
+{
+    System.Console.WriteLine("Первое число самое большое");
+    }
+else
+{
+    if (b > a && b > c) 
+{
+    System.Console.WriteLine("Второе число самое большое");
+    }
+else
+{
+     System.Console.WriteLine("Третье число самое большое");
+}
+}
