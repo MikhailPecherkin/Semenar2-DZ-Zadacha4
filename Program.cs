4ad5bed3753c38e44183c88a85644f9fe0c039d0
MikhailPecherkin <m.pechiorkin@yandex.ru>
2023-02-02 18:59:41 +0300

    Correct na odinakovie

--- Program.cs
+++ Program.cs
@@ -13,14 +13,27 @@ if (a > b && a > c)
 {
     System.Console.WriteLine($"Первое число самое большое:{a}");
     }
-else
-{
-    if (b > a && b > c) 
-{
-    System.Console.WriteLine($"Второе число самое большое:{b}");
+    else if (b > a && b > c) 
+    {
+        System.Console.WriteLine($"Второе число самое большое:{b}");
     }
+        else if (c > a && b < c) 
+        {
+            System.Console.WriteLine($"Третье число самое большое:{c}");
+        }
+            else if (a == b && a > c) 
+            {
+                System.Console.WriteLine($"Первое и второе число одинаково и самое большое:{a}");
+            }
+                else if (a == c && a > b) 
+                {
+                    System.Console.WriteLine($"Первое и третье число одинаково и самое большое:{a}");
+                }
+                    else if (b == c && a < b) 
+                    {
+                        System.Console.WriteLine($"Второе и третье число одинаково и самое большое:{b}");
+                    }
 else
 {
-     System.Console.WriteLine($"Третье число самое большое: {c}");
-}
+     System.Console.WriteLine($"Числа равны");
 }
